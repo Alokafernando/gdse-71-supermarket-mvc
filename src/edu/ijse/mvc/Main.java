0eaadc8dc026ed3663f5c532bc3fdf8aa77037e9
Alokafernando <buddhikaaloka2005@gmail.com>
2024-09-11 11:02:08 -0700

    config DBConnection

--- src/edu/ijse/mvc/Main.java
+++ src/edu/ijse/mvc/Main.java
@@ -2,11 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package edu.jse.mvc;
+package edu.ijse.mvc;
+
 
 /**
  *
- * @author user
+ * @author BUDDHIKA
  */
 public class Main {
 
@@ -14,7 +15,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      
     }
-    
 }
